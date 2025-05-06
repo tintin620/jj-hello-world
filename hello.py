bc4ecbc4d751b0d445f11323422d4699d0544a68
Hannes <juetting@gmail.com>
2025-05-06 10:26:08 +0200

    only print hello world

--- hello.py
+++ hello.py
@@ -2,6 +2,9 @@
 # This is the best implementation of this program to ever exist.
 # This is the second best.
 
-print("Hello World!")
-print("Goodbye")
+def main():
+  print("Hello World!")
 
+
+if __name__ == '__main__':
+    main()
