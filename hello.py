cd301c1e7dde113303ac6886055552ba0acc6b6d
Hannes <juetting@gmail.com>
2025-05-06 10:44:59 +0200

    create hello and goodbye functions

--- hello.py
+++ hello.py
@@ -2,9 +2,17 @@
 # This is the best implementation of this program to ever exist.
 # This is the second best.
 
-def main():
-  print("Hello World!")
 
+def print_hello():
+    print("Hello World!")
+
+def print_bye():
+    print("Goodbye!")
+
+    
+def main():
+    print_hello()
+    print_bye()
 
 if __name__ == '__main__':
     main()
